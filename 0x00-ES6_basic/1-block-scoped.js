36cceab0106f993086d7d1d04ebbfe39231ece07
EzechielKiregha <ekiregha@gmail.com>
2024-07-24 00:01:03 +0000

    No Used Vars | anonymous function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
-	let task2 = true;
+  const task = false;
+  const task2 = true;
 
-  	if (trueOrFalse) {
-    		let task = true;
-    		let task2 = false;
-  	}
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
+  }
 
-  	return [task, task2];
+  return [task, task2];
 }
