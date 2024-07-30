dafda677f9aab1b21aeae64f523285da499a0276
EzechielKiregha <ekiregha@gmail.com>
2024-07-31 01:44:32 +0200

    Clean set

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+/**
+ * a function that returns a map datastructure of
+ * grocceries
+ */
+
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
