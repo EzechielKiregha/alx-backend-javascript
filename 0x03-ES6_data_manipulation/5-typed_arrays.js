52a6f78d8ff7ee40d31ab17793d0fbbdbeef4e8a
EzechielKiregha <ekiregha@gmail.com>
2024-07-31 01:31:39 +0200

    typed array return a buffer with a value on specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+/**
+ * typed array return a buffer with a value on
+ * a specific position
+ */
+
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
