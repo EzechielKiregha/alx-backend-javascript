ed185bc68c129be7abf6f08cbe433694af6598e6
EzechielKiregha <ekiregha@gmail.com>
2024-07-30 18:48:24 +0200

    Function that create three different objects with atleast three properties

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents(){
+
+    const obj_1 = {
+        id : 1,
+        fistName : 'Guillaume',
+        location : 'San Francisco'
+    };
+    const obj_2 = {
+        id : 2,
+        fistName : 'James',
+        location : 'Columbia'
+    };
+    const obj_3 = {
+        id : 3,
+        fistName : 'Serena',
+        location : 'San Francisco'
+    };
+
+    return [obj_1, obj_2, obj_3]
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,3 @@
+## Data Manipulation Project
+
+in this directory we cover javascript basics of Data Manipulation, Objects, Typed Arrays or not. 
